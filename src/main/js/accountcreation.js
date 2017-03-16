cd332f2f47ec7c2cc839bbad75f756358a0efae1
Martin <mvieir4@uwo.ca>
2017-03-16 11:16:59 -0400

    FaceSpace tutorial

--- src/main/js/accountcreation.js
+++ src/main/js/accountcreation.js
@@ -1,5 +1,55 @@
 import React from 'react';
 
+var AccountComponent = React.createClass({
+
+    // Used to initialize state
+    getInitialState () {
+        return {
+            name : "",
+            success : ""
+        }
+    },
+
+    handleNameChange (e) {
+        // Prevent following the link.
+        e.preventDefault();
+        this.setState({ name : e.target.value , success : "..."});
+    },
+
+    handleSubmit(e) {
+        // Prevents reinitialization
+        e.preventDefault();
+        let name = this.state.name;
+        fetch('http://localhost:8080/AccountCreation/createAccount?userName=' + name, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res =>{
+            if(res.ok){
+                this.setState({success: 'Acccount created'});
+            }
+            else{
+                this.setState({success: 'Name already taken'});
+            }
+        })
+    },
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text" defaultValue={this.state.name} onChange={this.handleNameChange}/>
+                    </label>
+                    <input type="submit" value="Create a new Account" />
+                </form>
+                Success: {this.state.success}
+            </div>
+        );
+    }
+});
+
 export class AccountCreation extends React.Component {
 
     constructor() {
@@ -9,7 +59,10 @@ export class AccountCreation extends React.Component {
     render() {
         return(
             <div>
-                Account creation elements go here.
+                Create a new Account:
+                <br/>
+                <AccountComponent callback={this.setNameState}/>
+                <br/>
             </div>
         );
     }
